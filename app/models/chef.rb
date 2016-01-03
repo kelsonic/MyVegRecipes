d709ffbbdbd72569253d54acde912895db42e900
kgadams93 <kgadams93@gmail.com>
2016-01-03 17:25:56 +0000

    Create chefs and recipes association and tests

---
+++ app/models/chef.rb
@@ -0,0 +1,12 @@
+class Chef < ActiveRecord::Base
+  
+  has_many :recipes
+  
+  before_save { self.chefname = chefname.capitalize }
+  before_save { self.email = email.downcase }
+  
+  validates :chefname, presence: true, length: { minimum: 2, maximum: 40 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
+  
+end
