0a392aa4ef51062cf6999ee63f86e687381b56a5
kgadams93 <kgadams93@gmail.com>
2016-01-04 14:16:21 +0000

    Implement image upload for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,48 +1,46 @@
 class RecipesController < ApplicationController
-  
+ 
   def index
-    @recipes = Recipe.all
+	  @recipes = Recipe.all
   end
-  
+ 
   def show
-    @recipe = Recipe.find(params[:id])
+	  @recipe = Recipe.find(params[:id])
   end
-  
+ 
   def new
-    @recipe = Recipe.new
+	  @recipe = Recipe.new
   end
-  
+ 
   def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
-    
-    if @recipe.save
-      flash[:success] = "Your recipe was created successfully!"
-      redirect_to recipes_path
-      
-    else
-      render :new
-    end
+  	@recipe = Recipe.new(recipe_params)
+  	@recipe.chef = Chef.find(2)
+  	if @recipe.save
+    	flash[:success] = "Your recipe was created succesfully!"
+    	redirect_to recipes_path
+   	 
+  	else
+    	render :new
+  	end
   end
-  
+ 
   def edit
-    @recipe = Recipe.find(params[:id])
+	  @recipe = Recipe.find(params[:id])
   end
-  
+ 
   def update
-    @recipe = Recipe.find(params[:id])
-    if @recipe.update(recipe_params)
-      flash[:success] = "Your recipe was updated successfully!"
-      redirect_to recipe_path(@recipe)
-    else
-      render :edit
-    end
+  	@recipe = Recipe.find(params[:id])
+  	if @recipe.update(recipe_params)
+    	flash[:success] = "Your recipe was updated succesfully!"
+    	redirect_to recipe_path(@recipe)
+  	else
+    	render :edit
+  	end
   end
-  
+ 
   private
-    
-    def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description)
-    end
-  
+  	def recipe_params
+    	params.require(:recipe).permit(:name, :summary, :description, :picture)
+  	end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,16 @@ class Recipe < ActiveRecord::Base
   belongs_to :chef
   
   validates :chef_id, presence: true
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :summary, presence: true, length: { minimum: 10, maximum: 150 }
-  validates :description, presence: true, length: { minimum: 20, maximum: 500 }
+  validates :name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :summary, presence: true, length: {minimum: 10, maximum: 150}
+  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  mount_uploader :picture, PictureUploader
+  validate :picture
+ 
+  private
+  	def picture_size
+    	if picture.size > 5.megabytes
+      	errors.add(:picture, "Should be less than 5MB")
+    	end
+  	end
 end
