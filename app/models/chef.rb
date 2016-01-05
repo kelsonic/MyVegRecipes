fbb6c33b65ace9d513a35fdebcac35d43c2a4626
kgadams93 <kgadams93@gmail.com>
2016-01-05 18:52:44 +0000

    Build chef authentication system and register new chef form

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,4 +9,6 @@ class Chef < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
   
+  has_secure_password
+  
 end
