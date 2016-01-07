690221d5bac70bdfe52254cbe261533cf05f3e3b
kgadams93 <kgadams93@gmail.com>
2016-01-07 13:04:03 +0000

    Add styles and ingredient partials and pages

--- config/routes.rb
+++ config/routes.rb
@@ -29,4 +29,8 @@ Rails.application.routes.draw do
   post '/login', to: 'logins#create'
   get '/logout', to: 'logins#destroy'
   
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
+  
+  
 end
