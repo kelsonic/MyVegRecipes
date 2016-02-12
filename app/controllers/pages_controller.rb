9b44150caf2dc2ad12c8d204140ec0b7e9de985a
kgadams93 <kgadams93@gmail.com>
2016-02-12 16:43:33 +0000

    Delete contact page and edit about page copy and footer copy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,4 @@ class PagesController < ApplicationController
   def about
   end
   
-  def contact
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
-  get '/contact', to: 'pages#contact'
   
   resources :recipes do
     member do
